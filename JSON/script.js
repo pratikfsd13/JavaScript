cb54318431c64d9480704b2b6d6ebebb4b939309
pratikfsd13 <inosent.pratik@gmail.com>
2026-07-11 18:54:54 +0530

    21 June

---
+++ JSON/script.js
@@ -0,0 +1,16 @@
+const input = document.querySelector("#taskInput");
+const todoList = document.querySelector("#todoList");
+
+function addTask() {
+    //validation
+    if (input.value.trim() === "") {
+        alert("Please enter a valid task.");
+        return;
+    } 
+    //create new li element
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    
+    todoList.appendChild(li);
+    input.value = "";   
+}
