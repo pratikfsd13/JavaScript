13ff7b80132f41d9ce8687fbe1409f9c247eeecf
pratikfsd13 <inosent.pratik@gmail.com>
2026-07-18 16:04:58 +0530

    21 June

--- JSON/script.js
+++ JSON/script.js
@@ -6,11 +6,30 @@ function addTask() {
     if (input.value.trim() === "") {
         alert("Please enter a valid task.");
         return;
-    } 
+    }
     //create new li element
     const li = document.createElement("li");
     li.textContent = input.value;
-    
+
+    const completeBtn = document.createElement("button");
+    completeBtn.textContent = "✅";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+
+    li.append("", completeBtn, "", deleteBtn);
     todoList.appendChild(li);
-    input.value = "";   
-}
+
+    //mark task as complete
+    completeBtn.addEventListener("click",() => {
+        li.classList.toggle("completed");
+    });
+
+    //delete task
+    deleteBtn.addEventListener("click",() => {
+        li.remove();
+    });
+
+    todoList.appendChild(li);
+    input.value = "";
+}   
